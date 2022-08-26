018dce0f98f963169219c10c6f90ec59865affbf
dngrvn <108631571+dngrvn@users.noreply.github.com>
2022-08-26 17:28:32 +0300

    Add files via upload

---
+++ Zadacha_66/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+int sum = 0;
+Console.Write("Enter M: ");
+
+int m = Convert.ToInt16(Console.ReadLine());
+Console.Write("Enter N: ");
+
+int n = Convert.ToInt16(Console.ReadLine());
+for (int i = m; i <= n; i++)
+    sum += i;
+Console.WriteLine(sum);
---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29*/
+
+
+
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Aker(n, m);
+static int Aker(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+
+    if (m != 0 && n == 0)
+        return Aker(m - 1, 1);
+
+    if (m > 0 && n > 0)
+        return Aker(m - 1, Aker(m, n - 1));
+    return Aker(m, n);
+}
+System.Console.WriteLine(Aker(m, n));
